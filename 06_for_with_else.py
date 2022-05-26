eb8e16ea5947f875cf65320cf4c9934005e07c36
sahil78272026 <sahil.garg78272026@gmail.com>
2022-05-26 20:15:09 +0530

    Add files via upload

---
+++ 06_for_with_else.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print("Print i ", i)
+
+else: print("Printing else after for loop")
+
+print("\n")
+
+
+i = 0
+while i <10:
+    print(i)
+    i=i+1
+
+else :  print("else in while loop")
